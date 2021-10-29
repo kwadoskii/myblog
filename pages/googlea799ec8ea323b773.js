b74ea5dcdd0fdc414215d1198ab39070b2ae48be
Austin Ofor <kwadoskii@live.com>
2021-10-29 08:00:42 +0100

    SEO 3

---
+++ pages/googlea799ec8ea323b773.js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function googlea799ec8ea323b773() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/googlea799ec8ea323b773.html");
+  }, []);
+
+  return null;
+}
