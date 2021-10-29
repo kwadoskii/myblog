332436cc80ca9451247af34021abd4707f2defca
Austin Ofor <kwadoskii@live.com>
2021-10-29 08:03:17 +0100

    SEO 4

--- pages/googlea799ec8ea323b773.js
+++ pages/googlea799ec8ea323b773.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function googlea799ec8ea323b773() {
+export default function GooglePage() {
   const router = useRouter();
 
   useEffect(() => {
