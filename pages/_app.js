e2a761e2a5b3573f5c700030f1921928aa3d7b32
Austin Ofor <kwadoskii@live.com>
2021-10-30 17:02:28 +0100

    add support for route change indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
-import Header from "../components/Header";
-import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+import Header from "../components/Header";
+
+import "../styles/globals.css";
+
+const progress = new ProgressBar({
+  size: 5,
+  color: "#a5c3f5eb",
+  delay: 100,
+  className: "z-[60]",
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
